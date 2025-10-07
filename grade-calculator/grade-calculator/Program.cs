9674cdbca0d866c9c8d767d5ce5ead44334be1c2
mksigrest <mksigrest@gmail.com>
2025-10-07 08:31:08 -0400

    added average that is added to

--- grade-calculator/grade-calculator/Program.cs
+++ grade-calculator/grade-calculator/Program.cs
@@ -18,14 +18,41 @@ grades[6] = 82;
 grades[7] = 75;
 grades[8] = 67;
 grades[9] = 88;
+float average = 0;
 
 Console.WriteLine("Welcome " + name);
-Console.Write("Here are your grades: ");
+Console.WriteLine("Here are your grades: ");
 for (int i = 0; i < 10; i++)
 {
-    if (i == 9)
+    Console.Write(grades[i]);
+    if (grades[i] >= 90 && grades[i] <= 100)
     {
-        Console.WriteLine(grades[i] + ".");
+        Console.WriteLine("This grade is an A!");
+        if (grades[i] == 100)
+        {
+            Console.WriteLine("WOW! A perfect score!");
+        }
     }
-    Console.Write(grades[i] + ", ");
+    else if (grades[i] >= 80 && grades[i] < 90)
+    {
+        Console.WriteLine("This grade is an B");
+    }
+    else if (grades[i] >= 70 && grades[i] < 80)
+    {
+        Console.WriteLine("This grade is an C");
+    }
+    else if (grades[i] >= 65 && grades[i] < 70)
+    {
+        Console.WriteLine("This grade is an D");
+    }
+    else if (grades[i] >= 0 && grades[i] < 65)
+    {
+        Console.WriteLine("This grade is an F");
+    }
+    else
+    {
+        Console.WriteLine("This is out of the range of 0-100. How did you even get this grade?");
+    }
+
+    average += grades[i];
 }
