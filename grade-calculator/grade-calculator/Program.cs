f32a42b288d7011cdba2ee89d358bb406c82853a
mksigrest <mksigrest@gmail.com>
2025-10-07 08:21:40 -0400

    init code
    code push up until printing grades

---
+++ grade-calculator/grade-calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Max Sigrest
+ * IGME 201, tue/thur 8:00 am
+ * October 7th, 2025
+ */
+
+using System.ComponentModel.DataAnnotations;
+
+string name = "Max Sigrest";
+int[] grades = new int[10];
+grades[0] = 100;
+grades[1] = 92;
+grades[2] = 87;
+grades[3] = 200;
+grades[4] = -20;
+grades[5] = 52;
+grades[6] = 82;
+grades[7] = 75;
+grades[8] = 67;
+grades[9] = 88;
+
+Console.WriteLine("Welcome " + name);
+Console.Write("Here are your grades: ");
+for (int i = 0; i < 10; i++)
+{
+    if (i == 9)
+    {
+        Console.WriteLine(grades[i] + ".");
+    }
+    Console.Write(grades[i] + ", ");
+}
